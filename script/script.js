015068f829f661b467ce1af160a56af136145db5
Ilya Libin <ilyali911@gmail.com>
2022-01-15 16:07:39 +0200

    fixing HTML tags, buttons files, JS file

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let popupForm = document.querySelector("#editButton");
 let showPopup = document.querySelector("#popup");
-let closePopup = document.querySelector(".btn__profile-close");
+let closePopup = document.querySelector(".profile__btn_close");
 let heroName = document.querySelector("#heroName");
 let heroTitle = document.querySelector("#heroTitle");
 let form = document.querySelector(".popup__content");
@@ -16,8 +16,14 @@ function hideForm() {
 function changeName(evt) {
   let inputName = document.querySelector("#inputName");
   let inputTitle = document.querySelector("#inputTitle");
-  heroName.innerText = inputName.value;
-  heroTitle.innerText = inputTitle.value;
+  heroName.textContent = inputName.value;
+  heroTitle.textContent = inputTitle.value;
+  if (inputName.value.length === 0) {
+    return inputName;
+  }
+  if (inputTitle.value.length === 0) {
+    return inputTitle;
+  }
   showPopup.classList.add("popup_active");
   evt.preventDefault();
 }
